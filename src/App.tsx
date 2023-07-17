4cbdc74d4db28c54b48f2b0ab53d4013fe9aa38a
rhani <rhani@HENRYS-LAPTOP>
2023-07-17 14:33:45 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { FiberContainer } from "./FiberContainer"
 function App() {
   return (
     <div className="w-full h-full ">
+      <p className="absolute z-30 m-2 text-xl text-white">Cartoon Shader v1
+      </p>
        <FiberContainer />
     </div>
   )
