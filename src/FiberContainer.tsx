9ee47c57a58b00a77f38576bca6465dd4a111c0b
rhani <rhani@HENRYS-LAPTOP>
2023-07-31 17:12:24 +0100

    post processing

--- src/FiberContainer.tsx
+++ src/FiberContainer.tsx
@@ -4,6 +4,7 @@ import { OrbitControls } from "@react-three/drei";
 import { Scene } from "./Scene";
 import { Color } from "three";
 import { useControls, folder } from "leva";
+import { EffectComposer, Bloom, Pixelation, Scanline } from "@react-three/postprocessing";
 
 export function FiberContainer() {
   const { High, High_Mid, Low_Mid, Low, Background } = useControls("Color", {
@@ -14,7 +15,7 @@ export function FiberContainer() {
       Low: "#8000ff",
     }),
     Background: folder({
-      Background: '#ffffff',
+      Background: "#ffffff",
     }),
   });
 
@@ -23,19 +24,19 @@ export function FiberContainer() {
       value: 0.98,
       min: 0,
       max: 1,
-      step: 0.01
+      step: 0.01,
     },
     Midtones: {
       value: 0.35,
       min: 0,
       max: 1,
-      step: 0.01
+      step: 0.01,
     },
     Shadows: {
       value: 0.0,
       min: 0,
       max: 1,
-      step: 0.01
+      step: 0.01,
     },
   });
 
@@ -48,21 +49,35 @@ export function FiberContainer() {
     ],
     [High, High_Mid, Low_Mid, Low]
   );
-  
+
   const brightnessThresholds = useMemo(
     () => [Highlights, Midtones, Shadows],
     [Highlights, Midtones, Shadows]
   );
 
   return (
-    <div style={{background: Background}} className=" justify-self-center absolute h-full w-full">
+    <div
+      style={{ background: Background }}
+      className=" justify-self-center absolute h-full w-full"
+    >
       <Canvas camera={{ position: [50, 4, 0], fov: 30 }} shadows>
         <Scene colors={colors} brightnessThresholds={brightnessThresholds} />
-        <OrbitControls 
-          minDistance={10} 
+        <OrbitControls
+          minDistance={10}
           maxDistance={100}
           enablePan={false}
-          enableRotate={true} />
+          enableRotate={true}
+        />
+        <EffectComposer>
+          <Bloom
+            luminanceThreshold={0.3}
+            luminanceSmoothing={0}
+            height={300}
+            intensity={1}
+          />
+          <Pixelation 
+            granularity={0} />
+        </EffectComposer>
       </Canvas>
     </div>
   );
